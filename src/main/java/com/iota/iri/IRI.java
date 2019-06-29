7965ad7c9e75e1fe46964dda1645e62cab3568e8
Luca Moser <moser.luca@gmail.com>
2019-06-29 12:51:21 +0200

    bumps version number

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -43,7 +43,7 @@ import org.slf4j.LoggerFactory;
 public class IRI {
 
     public static final String MAINNET_NAME = "IRI";
-    public static final String TESTNET_NAME = "IRI ICC 0.1.30";
+    public static final String TESTNET_NAME = "IRI ICC 0.1.31";
 
     /**
      * The entry point of IRI.
